85056415d4a0c1c5aec07f2fd50d5b0dc9091fc9
unknown <phly95>
2019-09-12 17:59:57 -0400

    another edit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+// From https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
