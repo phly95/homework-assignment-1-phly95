722956b5c6de9504e5802911cb1f981385db1669
unknown <phly95>
2019-09-12 18:09:41 -0400

    lalala

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 // From https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+
+var audio = document.getElementById("music");
+audio.volume = 0.1;
+// From https://stackoverflow.com/questions/33747398/html-audio-tag-volume
