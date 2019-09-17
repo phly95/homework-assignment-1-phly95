ec63e490cedc04acbfe5c7f1b50c7991eb26ed10
unknown <phly95>
2019-09-16 20:07:48 -0400

    fox theif

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,15 @@ myHeading.textContent = 'Hello world!';
 var audio = document.getElementById("music");
 audio.volume = 0.1;
 // From https://stackoverflow.com/questions/33747398/html-audio-tag-volume
-
+let foxy = "this fox steals text"
 let finisher = document.getElementById(99);
-finisher.innerHTML = '<img src="images/foxtake.gif"></img>';
+finisher.innerHTML = '<img src="images/foxtake.gif" onmouseover="alert(foxy);"></img>';
+// finisher.onmouseenter = (alert('this fox steals text'));
+
+alert('👩‍💻Welcome to the website!🐔 visitor ' + vis());
+
+let visitor = vis();
+
+function vis() {
+    return (new Date().getMinutes() * 100) + new Date().getSeconds();
+}
